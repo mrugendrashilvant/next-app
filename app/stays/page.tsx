6105c11faa5ef15d7ea350ca42bd5627c08043bb
Mrugendra <mrugendra.s@reward360.co>
2026-09-25 17:00:07 +0530

    changes

--- app/stays/page.tsx
+++ app/stays/page.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
 export default function Stays() {
     return (
-        <p>Stays</p>
+        <>
+            <div className="hero bg-base-100">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <div className="card bg-base-100 w-96 border mb-4">
+                            <div className="card-body">
+                                <h2 className="card-title">Card title!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                            </div>
+                        </div>
+                        <Link href={'/stays/list'} className="btn btn-primary">Search</Link>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
